3152ea539eb46b299a2e708974d5c7c2654d8bc6
Reid Vandewiele <reid@puppetlabs.com>
2014-01-29 09:52:36 -0800

    Remove middleware, implement contain
    Cleanup and clearing out of unused paths in prep for using for PE

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,16 +3,6 @@ class mcollective (
   # which subcomponents to install here
   $server = true,
   $client = false,
-  $middleware = false,
-
-  # middleware tweaking
-  $activemq_template = 'mcollective/activemq.xml.erb',
-  $activemq_console = false, # ubuntu why you no jetty.xml!
-  $activemq_config = undef,
-  $activemq_confdir = $mcollective::defaults::activemq_confdir,
-  $rabbitmq_confdir = '/etc/rabbitmq',
-  $rabbitmq_vhost = '/mcollective', # used by rabbitmq
-  $delete_guest_user = false,
 
   # installing packages
   $manage_packages = true,
@@ -61,28 +51,14 @@ class mcollective (
   $ssl_server_private = undef,
   $ssl_client_certs = 'puppet:///modules/mcollective/empty',
 ) inherits mcollective::defaults {
-  anchor { 'mcollective::begin': }
-  anchor { 'mcollective::end': }
 
   if $client or $server {
-    # We don't want this on middleware roles.
-    Anchor['mcollective::begin'] ->
-    class { '::mcollective::common': } ->
-    Anchor['mcollective::end']
+    contain mcollective::common
   }
   if $client {
-    Anchor['mcollective::begin'] ->
-    class { '::mcollective::client': } ->
-    Anchor['mcollective::end']
+    contain mcollective::client
   }
   if $server {
-    Anchor['mcollective::begin'] ->
-    class { '::mcollective::server': } ->
-    Anchor['mcollective::end']
-  }
-  if $middleware {
-    Anchor['mcollective::begin'] ->
-    class { '::mcollective::middleware': } ->
-    Anchor['mcollective::end']
+    contain mcollective::server
   }
 }
