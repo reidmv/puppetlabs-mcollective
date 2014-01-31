c17f018d26e0ac004c7d863d7260197c025062ff
Reid Vandewiele <reid@puppetlabs.com>
2014-01-31 09:19:05 -0800

    Replace facts.yaml pattern with cron job
    Because there is no way to iterate over all possible mutable facts, and
    it's better to refresh more frequently than Puppet may run.
    
    Note that this was tested against PE, but written in a way that should
    be agnostic to PE/POSS.

--- manifests/server/config/factsource/yaml.pp
+++ manifests/server/config/factsource/yaml.pp
@@ -4,13 +4,28 @@ class mcollective::server::config::factsource::yaml {
     fail("Use of private class ${name} by ${caller_module_name}")
   }
 
-  # This pattern originally from
-  # http://projects.puppetlabs.com/projects/mcollective-plugins/wiki/FactsFacterYAML
-  file { $mcollective::yaml_fact_path:
-    owner   => 'root',
+  $mco_etc = $mcollective::configdir
+
+  # Template uses:
+  #   - $mco_etc
+  file { "${mcollective::core_libdir}/refresh-mcollective-metadata":
+    owner   => '0',
     group   => '0',
-    mode    => '0400',
-    content => template('mcollective/facts.yaml.erb'),
+    mode    => '0755',
+    content => template('mcollective/refresh-mcollective-metadata.erb'),
+    before  => Cron['refresh-mcollective-metadata'],
+  }
+  cron { 'refresh-mcollective-metadata':
+    environment => "PATH=/opt/puppet/bin:${::path}",
+    command     => "${mcollective::core_libdir}/refresh-mcollective-metadata",
+    user        => 'root',
+    minute      => [ '0', '15', '30', '45' ],
+  }
+  exec { 'create-mcollective-metadata':
+    path    => "/opt/puppet/bin:${::path}",
+    command => "${mcollective::core_libdir}/refresh-mcollective-metadata",
+    creates => "${mco_etc}/facts.yaml",
+    require => File["${mcollective::core_libdir}/refresh-mcollective-metadata"],
   }
 
   mcollective::server::setting { 'factsource':
